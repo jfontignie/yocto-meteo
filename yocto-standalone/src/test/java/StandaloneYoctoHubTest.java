7566b7194814e8c095aac0197f3586fb967b9a88
jfontignie <jacques.fontignie@gmail.com>
2012-04-23 22:24:36 +0200

    Add the tests of yoctotemplate

--- yocto-standalone/src/test/java/StandaloneYoctoHubTest.java
+++ yocto-standalone/src/test/java/StandaloneYoctoHubTest.java
@@ -10,6 +10,7 @@
  * You should have received a copy of the GNU General Public License along with yocto-meteo. If not, see http://www.gnu.org/licenses/.
  */
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -27,13 +28,13 @@ public class StandaloneYoctoHubTest {
 
     private YoctoHub hub;
 
-    //@Before
+    @Before
     public void setUp() throws IOException {
         YoctoTemplate template = new StandaloneYoctoTemplate(new URL("http://127.0.0.1:4444"));
         hub = new YoctoHub(template);
     }
 
-    //@Test
+    @Test
     public void testMeteo() throws IOException {
         Collection<YoctoObject> objects = hub.findAll(YoctoProduct.YOCTO_METEO);
         for (YoctoObject object : objects) {
@@ -43,7 +44,7 @@ public class StandaloneYoctoHubTest {
         }
     }
 
-    //@Test
+    @Test
     public void testLoop() throws IOException, InterruptedException {
         for (int i = 0; i < 10; i++) {
             Collection<YoctoObject> objects = hub.findAll(YoctoProduct.YOCTO_METEO);
@@ -57,7 +58,7 @@ public class StandaloneYoctoHubTest {
         }
     }
 
-    //@Test
+    @Test
     public void testLoop2() throws IOException, InterruptedException {
         Collection<YoctoObject> objects = hub.findAll(YoctoProduct.YOCTO_METEO);
         for (int i = 0; i < 10; i++) {
